abff05debdf29fbebd7d86dbfd708dc8c7376412
Nathan Everett <43852724+neverett8fr@users.noreply.github.com>
2021-06-05 11:43:52 +0100

    Add files via upload

---
+++ websiteIndexer-Python/main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+file_size = input("Small(s), Medium(m) or Big(b): ")
+website_url = input("Enter a website URL (include https://): ")
+sub_dir = input("Search subdirectories (y/n)")
+
+
+
+
+print("Finished!")
